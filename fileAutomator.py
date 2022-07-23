ab25909bcd24ea9170ea31a4992011c53dc7698d
Jai Amin <79227536+jamino30@users.noreply.github.com>
2022-07-23 01:10:48 -0400

    Update fileAutomator.py

--- fileAutomator.py
+++ fileAutomator.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog as fd
 from os.path import exists, expanduser
 from shutil import move
 from file_types import *
@@ -6,21 +8,39 @@ from file_types import *
 import binascii
 import os
 
+# setup tkinter GUI
 win = Tk()
 win.title("FileOS - A modern file manager")
-win.geometry("750x250")
+win.geometry("700x250")
+win.resizable(False, False)
 
-label = Label(win, text="FileOS - A modern file manager for Windows/macOS")
-label.pack(pady=20)
+# style widgets
+s = ttk.Style()
+s.configure(".", font=("Lato", 15, "bold"))
 
-entry = Entry(win, width=20)
-entry.focus_set()
-entry.pack(pady=20)
+heading1 = ttk.Label(win, text="FileOS", font="Lato 30 bold")
+heading1.pack(pady=(50, 0))
+
+heading2 = ttk.Label(win, text="A modern file manager for Windows/macOS", font="Lato 17 bold")
+heading2.pack(pady=(10, 0))
+
+get_dir_label = ttk.Label(win, text="1. ", font="Lato 15 bold")
+get_dir_label.pack(padx=(80, 5), side="left")
+
+
+# get desired directory
+def get_dir():
+    global dir_name
+    dir_name = fd.askdirectory()
+
+
+get_dir = ttk.Button(win, text="Choose a directory", width=15, command=get_dir)
+get_dir.pack(side="left")
 
 
 def file_manager():
-    # finds user home directory and gets desired directory to be organized
-    parent_dir = expanduser("~") + "/" + entry.get() + "/"
+    # finds user home directory and gets desired directory
+    parent_dir = dir_name + "/"
     dir_files = os.listdir(parent_dir)
 
     # ignore .DS_Store and .localized Mac files
@@ -30,35 +50,42 @@ def file_manager():
         dir_files.pop(dir_files.index(".localized"))
 
     for file in dir_files:
-        full_file = parent_dir + file
-
-        # open and read each file in binary format
-        with open(full_file, "rb") as f:
-            content = f.read()
-
-        # convert file binary to hex signatures
         try:
-            hex_bin = str(binascii.hexlify(content))[2:10].upper()
-        except KeyError:
-            print(f"Error reading the {file}")
+            full_file = parent_dir + file
 
-        # create a directory with the file and its respective extension
-        for item in bit_ext.keys():
-            if item in hex_bin:
-                directory = bit_ext[item]
-                path = os.path.join(parent_dir, directory)
+            # open and read each file in binary format
+            with open(full_file, "rb") as f:
+                content = f.read()
 
-                # if directory exists, then move the file into it
-                if exists(path):
-                    move(full_file, path)
-                # else create a new one first, then move the file into it
-                else:
-                    os.mkdir(path)
-                    move(full_file, path)
+            # convert file binary to hex signatures
+            try:
+                hex_bin = str(binascii.hexlify(content))[2:10].upper()
+            except KeyError:
+                print(f"Error reading the {file}")
+
+            # create a directory with the file and its respective extension
+            for item in bit_ext.keys():
+                if item in hex_bin:
+                    directory = bit_ext[item]
+                    path = os.path.join(parent_dir, directory)
+
+                    # if directory exists, then move the file into it
+                    if exists(path):
+                        move(full_file, path)
+                    # else create a new one first, then move the file into it
+                    else:
+                        os.mkdir(path)
+                        move(full_file, path)
+
+        except IsADirectoryError:
+            continue
 
 
-button = Button(win, text="Run FileOS", width=20, command=file_manager)
-button.pack(pady=20)
+# When button clicked, file_manager() script runs
+run_script = ttk.Button(win, text="Run FileOS", width=15, command=file_manager)
+run_script.pack(pady=40, padx=(5, 80), side="right")
 
+run_script_label = ttk.Label(win, text="2. ", font="Lato 15 bold")
+run_script_label.pack(side="right")
 
 win.mainloop()
