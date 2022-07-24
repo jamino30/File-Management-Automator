e0c8ccb8c5cff0cd5dfa656c571fd34d558531e7
Jai Amin <79227536+jamino30@users.noreply.github.com>
2022-07-24 16:32:03 -0400

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,9 +1,10 @@
-# FileMan libraries
+# NaviFile libraries
 
-from tkinter import ttk, filedialog, messagebox
+from tkinter import ttk, filedialog
 from os.path import exists
 from shutil import move
-from file_types import bit_ext
+from file_types import hexa_sig
+from subprocess import call
 
 import binascii
 import os
